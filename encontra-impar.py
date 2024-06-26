a226d524e56832c92d4e9ab2ac1b1c81da9036b7
leo <164808753+secboyleo@users.noreply.github.com>
2024-06-26 10:49:36 -0300

    Add files via upload

---
+++ encontra-impar.py
@@ -0,0 +1,7 @@
+def  encontra_impares(lista):
+  if len(lista) == 0:
+    return []
+  elif lista[0] % 2 != 0:
+    return [lista[0]] + encontra_impares(lista[1:])
+  else:
+    return encontra_impares(lista[1:])
